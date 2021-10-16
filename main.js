4d22882960dd90b3e1b7cb5dba7ee5be14909a71
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-10-16 09:58:44 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const server = express();
+var formidable = require('formidable');
+var app = express();
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(5000);
+/*
+app.listen(PORT,() => {
+	console.log('listening on ${PORT}')
+})
+*/
+app.use(express.urlencoded({
+	extended: true
+}))
+/*
+app.post('/', function (req, res){
+    var form = new formidable.IncomingForm();
+
+    form.parse(req);
+
+    form.on('fileBegin', function (name, file){
+        file.path = __dirname + '/uploads/' + file.name;
+    });
+
+    form.on('file', function (name, file){
+        console.log('Uploaded ' + file.name);
+    });
+
+    res.sendFile(__dirname + '/index.html');
+	
+});
+
+
+app.listen(PORT,() => {
+	console.log('listening on ${PORT}')
+})
+
+
+server.get('/', (req, res) => {
+	res.send('Hello World')
+})
+server.listen(PORT, () => {
+	console.log('listening on ${PORT}')
+})
+*/
