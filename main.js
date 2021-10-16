371f12d9368c3c9c1cac2af68b3757f50a9b2069
Brandon Miller <71044640+BrandonJMiller@users.noreply.github.com>
2021-10-16 10:10:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process = process.env.PORT || 5000
+
 const server = express();
-var formidable = require('formidable');
-var app = express();
-
-
 server.get('/', (req, res) => {
-	res.send('Hello World')
+	res.sent('Hello World')
 })
+
 server.listen(PORT, () => {
-	console.log('listening on ${PORT}')
+	console.log(`listening on ${PORT}`)
 })
 
